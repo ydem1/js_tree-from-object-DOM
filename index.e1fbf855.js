1db6b05c6e81cc8c413efb9850f779b316e89cf4
Vadym Bavorovskyi <vadymbavorovskiy@gmail.com>
2024-01-08 14:08:34 +0200

    make build

---
+++ index.e1fbf855.js
@@ -0,0 +1,2 @@
+!function e(t,n){let r=document.createElement("ul");for(let t of Object.keys(n)){let l=document.createElement("li");l.textContent=t,r.append(l),n[t]&&e(l,n[t])}t.append(r)}(document.querySelector("#tree"),{Drink:{Wine:{},Schnaps:{}},Fruit:{Red:{Cherry:{},Strawberry:{}},Yellow:{Banana:{},Pineapple:{}}}});
+//# sourceMappingURL=index.e1fbf855.js.map
